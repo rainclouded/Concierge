13411e6574283f4c722e145697577b3d28627cd7
LeeroyDilim <leeroydilim@gmail.com>
2024-10-08 18:57:33 -0500

    removed permission validator files for now, as it is not implemented for this sprint

--- src/incident_reports/incident_reports_server/application/services.py
+++ src/incident_reports/incident_reports_server/application/services.py
@@ -1,11 +1,8 @@
 from incident_reports_server.persistence.i_incident_report_persistence import IIncidentReportPersistence
 from incident_reports_server.persistence.incident_report_persistence_stub import IncidentReportPersistenceStub
-from incident_reports_server.validators.permission_validator import PermissionValidator 
-from incident_reports_server.validators.i_permission_validator import IPermissionValidator
 
 class Services:
     _incident_report_persistence = None
-    _permission_validator = None
     
     @staticmethod
     def get_incident_report_persistence() -> IIncidentReportPersistence:
@@ -14,15 +11,8 @@ class Services:
             
         return Services._incident_report_persistence
     
-    @staticmethod
-    def get_permission_validator() -> IPermissionValidator:
-        if Services._permission_validator is None:
-            Services._permission_validator = PermissionValidator()
-            
-        return Services._permission_validator
     
     @staticmethod
     def clear() -> None:
         Services._incident_report_persistence = None
-        Services._permission_validator = None
         
