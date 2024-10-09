cad7426e7ef669e6a9c1b0db2d0d0f96d99a69e7
Mykola <mykola.besarab@gmail.com>
2024-10-08 20:04:45 -0500

    amenities cypress test

--- src/guest_webapp/cypress/components/HomePage.cy.jsx
+++ src/guest_webapp/cypress/components/HomePage.cy.jsx
@@ -1,5 +1,5 @@
 import HomePage from '../../src/pages/HomePage';
-import { MemoryRouter } from 'react-router-dom';  // Import MemoryRouter for testing
+import { MemoryRouter } from 'react-router-dom'; 
 
 describe('HomePage Component Tests', () => {
   it('should render the HomePage component and display Quick Service', () => {
