77644667c1e5caeecc5c9fc37d386067e010ead8
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2024-10-12 23:53:54 -0500

    Fixed docker compose integration bug

--- src/amenities/amenities_server/application/Services.cs
+++ src/amenities/amenities_server/application/Services.cs
@@ -4,41 +4,49 @@ public static class Services
 
     public static IAmenityPersistence GetAmenityPersistence()
     {
-        _amenityPersistence??=ConfigureDB();
+        _amenityPersistence ??= ConstructAmenityPersistence();
         return _amenityPersistence;
     }
 
-    public static void clear(){
+    public static void clear()
+    {
         _amenityPersistence = null;
     }
-    
-    private static IAmenityPersistence ConfigureDB(){
-      string? dbImplentation = Environment.GetEnvironmentVariable("DB_IMPLEMENTATION");
-      return ConfigureDB(dbImplentation ?? "");
-    }
-    
-    private static IAmenityPersistence ConfigureDB(string dbImplentation)
+
+    private static IAmenityPersistence ConstructAmenityPersistence()
     {
-      IAmenityPersistence? amenityPersistence = null;
-      if (dbImplentation == "POSTGRES")
-      { 
-         Console.WriteLine("Attempting to connect to Postgres");
-         string? dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "";
-         string? dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "";
-         string? dbUsername = Environment.GetEnvironmentVariable("DB_USERNAME") ?? "";
-         string? dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "";
-         try
-         {
-            amenityPersistence = new PostgresAmenityPersistence($"Host={dbHost}; Port={dbPort}; Username={dbUsername}; Password={dbPassword}");
-            Console.WriteLine("Postgress Connected successfully");
-         }
-         catch (InvalidOperationException)
-         {
-            amenityPersistence = null; //use stub if connection failed
-            Console.WriteLine("Postgress Failed");
-         }
-      }
-      
-      return amenityPersistence ?? new StubAmenityPersistence();
+        string? dbImplementation = Environment.GetEnvironmentVariable("DB_IMPLEMENTATION");
+        return ConstructAmenityPersistence(dbImplementation ?? "");
+    }
+
+    private static IAmenityPersistence ConstructAmenityPersistence(string dbImplementation)
+    {
+        IAmenityPersistence? amenityPersistence = null;
+        if (dbImplementation == "POSTGRES")
+        {
+            Console.WriteLine("Attempting to connect to Postgres");
+
+            try
+            {
+                amenityPersistence = new PostgresAmenityPersistence(PostgresConnectionString());
+                Console.WriteLine("Postgress Connected successfully");
+            }
+            catch (InvalidOperationException)
+            {
+                amenityPersistence = null; //use stub if connection failed
+                Console.WriteLine("Postgress Failed");
+            }
+        }
+
+        return amenityPersistence ?? new StubAmenityPersistence();
+    }
+
+    private static string PostgresConnectionString()
+    {
+        string? dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "";
+        string? dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "";
+        string? dbUsername = Environment.GetEnvironmentVariable("DB_USERNAME") ?? "";
+        string? dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "";
+        return $"Host={dbHost}; Port={dbPort}; Username={dbUsername}; Password={dbPassword}";
     }
 }
