f2054daad36a4bdc97d8e22be61e268c600a1d45
rainclouded <155860244+rainclouded@users.noreply.github.com>
2024-10-17 17:42:14 -0500

    break test

--- src/amenities/amenities_test/UnitTest1.cs
+++ src/amenities/amenities_test/UnitTest1.cs
@@ -15,9 +15,9 @@ namespace amenities_test
         [SetUp]
         public void Setup()
         {
-            _testValidAmenity = new Amenity("testValidAmenity", "testValidDesc", new TimeSpan(0, 0, 0), new TimeSpan(12, 0, 0));
-            _testUpdatedValidAmenity = new Amenity("_testUpdatedValidAmenity", "testUpdatedValidDesc", new TimeSpan(12, 0, 0), new TimeSpan(24, 0, 0));
-            _testInvalidAmenity = new Amenity("testInvalidAmenity", "testInvalidDesc", new TimeSpan(13, 0, 0), new TimeSpan(12, 0, 0));
+            _testValidAmenity = new Amenity("testValiertdAmenity", "testVrtalidDesc", new TimeSpan(0, 0, 0), new TimeSpan(12, 0, 0));
+            _testUpdatedValidAmenity = new Amenity("_testUpdatedVretalidAmenity", "testUpdretatedValidDesc", new TimeSpan(12, 0, 0), new TimeSpan(24, 0, 0));
+            _testInvalidAmenity = new Amenity("testInvaliretdAmenity", "testIrtnvalidDesc", new TimeSpan(13, 0, 0), new TimeSpan(12, 0, 0));
 
             Services.clear();
             _amenityPersistence = Services.GetAmenityPersistence();
