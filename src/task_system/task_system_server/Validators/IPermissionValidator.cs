e5b0e6ac1395989d809c9619f9007799e3e2e07b
LeeroyDilim <leeroydilim@gmail.com>
2024-11-17 15:49:33 -0600

    more changes

--- src/task_system/task_system_server/Validators/IPermissionValidator.cs
+++ src/task_system/task_system_server/Validators/IPermissionValidator.cs
@@ -1,9 +1,9 @@
-﻿namespace task_system_server.Validators
+﻿using task_system_server.Models;
+
+namespace task_system_server.Validators
 {
     public interface IPermissionValidator
     {
-        public abstract bool ValidatePermissions(string permissionName, string sessionkey);
-        public abstract bool ValidateAccountId(int accountId, string sessionKey);
-
+        public abstract bool ValidatePermissions(string permissionName, string sessionkey, out SessionObj sessionData);
     }
 }
--- src/task_system/task_system_server/Validators/PermissionValidator.cs
+++ src/task_system/task_system_server/Validators/PermissionValidator.cs
@@ -1,22 +1,18 @@
-﻿namespace task_system_server.Validators
+﻿using task_system_server.Models;
+
+namespace task_system_server.Validators
 {
     public class PermissionValidator(Clients.PermissionClient permissionClient) : IPermissionValidator
     {
         private readonly Clients.PermissionClient _permClient = permissionClient;
 
-        public bool ValidatePermissions(string permissionName, string sessionKey)
+        public bool ValidatePermissions(string permissionName, string sessionKey, out SessionObj sessionData)
         {
             var permData = _permClient.GetSessionData(sessionKey).GetAwaiter().GetResult();
-            var permissionList = permData?.Data?.SessionData?.SessionPermissionList ?? [];
+            sessionData = permData?.Data?.SessionData;
+            var permissionList = sessionData?.SessionPermissionList ?? [];
 
             return permissionList.Contains(permissionName);
-        }
-
-        public bool ValidateAccountId(int accountId, string sessionKey)
-        {
-            var permData = _permClient.GetSessionData(sessionKey).GetAwaiter().GetResult();
-            
-            return permData?.Data?.SessionData?.AccountId == accountId;
         }
     }
 }
